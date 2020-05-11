6c52263eca46827a1904c8dce882cf2a7ec257c9
gxyau <GuoXian.Yau@gmail.com>
2020-05-11 16:09:13 +0200

    Trying to figure out how to use flux

--- data_modeling.jl
+++ data_modeling.jl
@@ -6,4 +6,6 @@ cd("/home/gxyau/Documents/github/who_suicide_rates/")
 
 # Loading required packages
 using CSV, DataFrames, Statistics # Simple statistical analysis
-using GLM, CategoricalArrays
+using GLM, CategoricalArrays, Flux
+
+data = CSV.read("stats_modeling_data.csv")
